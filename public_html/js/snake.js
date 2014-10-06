d20a828cd1b87c9843fdc839b290b0a005254cab
Aaron Peron <aaron.peron@yahoo.com>
2014-10-06 14:20:50 -0700

    codeing

---
+++ public_html/js/snake.js
@@ -0,0 +1,18 @@
+var snake;
+
+var context;
+var screenWidth;
+var screenHidth;
+
+function gameInitilalize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
