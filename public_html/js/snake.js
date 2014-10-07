00d4ef2867886b328921c0b5f2bb10a26dbe8c24
Aaron Peron <aaron.peron@yahoo.com>
2014-10-07 10:15:52 -0700

    coloring

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -4,9 +4,18 @@ var context;
 var screenWidth;
 var screenHidth;
 
+gameInitilalize();
+gameDraw();
+
 function gameInitilalize() {
     var canvas = document.getElementById("game-screen");
     context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHidth = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.hidth = screenHidth;
 }
 
 function gameLoop() {
@@ -14,5 +23,6 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    
+    context.fillstyle = "rgb(255, 25, 0)";
+    context.fillRect(0, 0, screenWidth, screenHidth);
 }
