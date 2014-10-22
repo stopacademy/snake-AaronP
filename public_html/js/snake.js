5902e482cfd63c3c864df4bdd9c9ba289eeabad1
Aaron Peron <aaron.peron@yahoo.com>
2014-10-22 14:05:29 -0700

    update

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -84,10 +84,12 @@ function snakeUpdate() {
     
     if(snakeDirection == "down") {
         snakeHeadY++;
-    }
+        }
     else if(snakeDirection == "right") {
         snakeHeadX++;
     }
+    
+    checkFoodCollision();
     
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
@@ -110,15 +112,15 @@ function foodInitialize() {
 
 function foodDraw() {
     context.fillStyle = "yellow";
-    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+    context.fillRect(food.x* snakeSize , food.y* snakeSize, snakeSize,snakeSize);
 }
 
 function setFoodPosition() {
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
     
-    food.x = randomX;
-    food.y = randomY;
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
 }
 
 /*----------------------------------------------------------------------------
@@ -129,10 +131,21 @@ function setFoodPosition() {
 function keyboardHandler(event) {
     console.log(event);
     
-    if(event.keyCode == "39") {
+    if(event.keyCode == "39" && snakeDirection != "left") {
         snakeDirection = "right";
     }
-    else if(event.keyCode == "") {
+    else if(event.keyCode == "40" && snakeDirection != "up") {
+        snakeDirection = "down";
+    }
+}
+
+/*----------------------------------------------------------------------------
+ * Collection  Handling
+ * ---------------------------------------------------------------------------
+ */
+
+function checkFoodCollision() {
+    if(snakeHead == food.x) {
         
     }
 }
